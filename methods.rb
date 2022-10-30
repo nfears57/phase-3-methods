bb61dded420e8f56aaa053c87d0dda970e14fb33
Nick <nfears5798@gmail.com>
2022-10-30 12:52:08 -0400

    commit tree 5892d8f52ab50f340f2a24513fc119da42f5e000

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer()
+   puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen!"
+greet_with_default
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(1, 3)
+
+def halve(num)
+    return nil unless num.class == Integer
+    
+    num / 2
+  end
+
